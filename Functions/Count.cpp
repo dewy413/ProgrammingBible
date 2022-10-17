72b9a4772822c35179b723d857fb63da84b9e09e
Jordan Deuley <114099341+dewy413@users.noreply.github.com>
2022-10-17 12:17:06 -0500

    Delete Count.cpp
    Moved to Functions/Strings

--- Functions/Count.cpp
+++
@@ -1,9 +0,0 @@
-int count(char* letters, char letter) {
-    int counter = 0;
-    for(int i = 0; i < sizeof(letters); i++) {
-        if(letters[i] == letter) {
-            counter++;
-        }
-    }
-    return counter;
-}
