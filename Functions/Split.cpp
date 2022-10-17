f8acce96cee27e01c352a71918bb56309002cd9f
Jordan Deuley <114099341+dewy413@users.noreply.github.com>
2022-10-17 12:18:11 -0500

    Delete Split.cpp
    Moved to Functions/Strings

--- Functions/Split.cpp
+++
@@ -1,7 +0,0 @@
-char* split(string word) {
-    char* wordsplit = new char[word.length()];
-    for(int i = 0; i < word.length(); i++) {
-        wordsplit[i] = word[i];
-    }
-    return wordsplit;
-}
