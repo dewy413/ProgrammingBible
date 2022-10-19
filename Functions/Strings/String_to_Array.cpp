02c75285f8c9b1f0a68e64033d87235ba123744b
Jordan Deuley <114099341+dewy413@users.noreply.github.com>
2022-10-19 11:50:40 -0500

    Create String_to_Array.cpp

---
+++ Functions/Strings/String_to_Array.cpp
@@ -0,0 +1,16 @@
+
+vector<string> string_to_array(const string& s) {
+    vector<string> ourvec;
+    string addedup;
+    for(auto i : s) {
+      switch(i) {
+        case(' '):
+          ourvec.push_back(addedup);
+          addedup = "";
+        default:
+          addedup += i; 
+      }
+    }
+    ourvec.push_back(addedup);
+    return ourvec;
+}
