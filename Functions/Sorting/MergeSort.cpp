342c7ddd8281883bee656719a22da06edbd82569
Jordan Deuley <114099341+dewy413@users.noreply.github.com>
2022-10-20 08:06:42 -0500

    Create MergeSort.cpp

---
+++ Functions/Sorting/MergeSort.cpp
@@ -0,0 +1,41 @@
+void merge(int B[], int bSize, int C[], int cSize, int A[]) {
+    int i, j, k;
+    i = 0;
+    j = 0;
+    k = 0;
+    while (i < bSize && j < cSize) {
+        if(B[i] <= C[j]) {
+            A[k] = B[i];
+            i++;
+        } else {
+            A[k] = C[j];
+            j++;
+        }
+        k++;
+    }
+    if(i == bSize) {
+        for(j = j; j < cSize; j++) {
+            A[k++] = C[j];
+        }
+    } else {
+        for (i = i; i < bSize; i++) {
+            A[k++] = B[i];
+        }
+    }
+}
+
+void mergeSort(int A[], double size) {
+    if(size > 1) {
+        int B[int(floor(size / 2))];
+        int C[int(ceil(size / 2))];
+        for(int i = 0; i < int(floor(size / 2)); i++) {
+            B[i] = A[i];
+        }
+        for(int i = int(floor(size / 2)); i < size; i++) {
+            C[i - int(floor(size / 2))] = A[i];
+        }
+        mergeSort(B, floor(size / 2));
+        mergeSort(C, ceil(size / 2));
+        merge(B, floor(size / 2), C, ceil(size / 2), A);
+    }
+}
